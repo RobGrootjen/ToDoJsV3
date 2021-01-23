83afc2be007047c0da35f8e4ec48e53f99955c4c
Rob Grootjen <robgrootjen1@gmail.com>
2021-01-23 10:46:25 -0600

    new update

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var clicker = document.getElementById('clicker');
-var outcome = docuement.getElementById('todos');
-var input = document.getElementById('maininput');
+var clickerButton = document.getElementById('clickersaso');
+var outcomeContainer = document.getElementById('todos');
+var inputContainer = document.getElementById('maininput');
 
-clicker.addEventListener('clicker', function(){
+clickerButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputContainer.value;
+    outcomeContainer.appendChild(paragraph);
+    inputContainer.value="";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+    })
+    paragraph.addEventListener('click', function(){
+        outcomeContainer.removeChild(paragraph);
+    })
+
+
     
 })
