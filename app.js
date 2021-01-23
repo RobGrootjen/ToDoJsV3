f142020af48afba5f194711ff2245d4820bc9f9e
Rob Grootjen <robgrootjen1@gmail.com>
2021-01-23 09:34:53 -0600

    new update

---
+++ app.js
@@ -0,0 +1,9 @@
+var clicker = document.getElementById('clicker');
+var outcome = docuement.getElementById('todos');
+var input = document.getElementById('maininput');
+
+clicker.addEventListener('clicker', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    
+})
